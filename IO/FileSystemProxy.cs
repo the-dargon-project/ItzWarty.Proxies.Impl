818830abdc504f6c56b64c7aa2b7f9c42b052199
Michael Yu <ItzWarty@gmail.com>
2014-11-16 18:11:27 -0800

    Initial Commit.

---
+++ IO/FileSystemProxy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ImpromptuInterface;
+
+namespace ItzWarty.IO
+{
+   public class FileSystemProxy : IFileSystemProxy
+   {
+      public IEnumerable<string> EnumerateDirectories(string path) { return Directory.EnumerateDirectories(path); }
+      public IEnumerable<string> EnumerateDirectories(string path, string searchPattern) { return Directory.EnumerateDirectories(path, searchPattern); }
+      public IEnumerable<string> EnumerateDirectories(string path, string searchPattern, SearchOption searchOption) { return Directory.EnumerateDirectories(path, searchPattern, searchOption); }
+
+      public IDirectoryInfo GetDirectoryInfo(string path) { return WrapDirectoryInfo(new DirectoryInfo(path)); }
+      public string ReadAllText(string path) { return File.ReadAllText(path); }
+      public void WriteAllText(string path, string contents) {  File.WriteAllText(path, contents);}
+      public void CopyFile(string sourceFilePath, string destinationFilePath) { File.Copy(sourceFilePath, destinationFilePath); }
+
+      public void PrepareDirectory(string path) { Directory.CreateDirectory(path); }
+      public void PrepareParentDirectory(string path) { Directory.CreateDirectory(path.Split('/', '\\').Pass(x => x.SubArray(0, x.Length - 1).Join("/"))); }
+
+      public void DeleteDirectory(string path, bool recursive = false) {
+         if (!recursive) {
+            Directory.Delete(path, false);
+         } else {
+            foreach (var dir in Directory.EnumerateDirectories(path)) {
+               DeleteDirectory(dir, true);
+            }
+            foreach (var file in Directory.EnumerateFiles(path)) {
+               File.SetAttributes(file, FileAttributes.Normal);
+               File.Delete(file);
+            }
+            Directory.Delete(path, false);
+         }
+      }
+
+      private IDirectoryInfo WrapDirectoryInfo(DirectoryInfo directoryInfo) { return directoryInfo.ActLike<IDirectoryInfo>(); }
+   }
+}
---
+++ Threading/ThreadingProxy.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using ImpromptuInterface;
+
+namespace ItzWarty.Threading {
+   public class ThreadingProxy : IThreadingProxy {
+      public IThread CreateThread(ThreadEntryPoint entryPoint, ThreadCreationOptions options) {
+         var thread = new Thread(() => entryPoint());
+         thread.IsBackground = options.IsBackground;
+         return thread.ActLike<IThread>();
+      }
+   }
+}
