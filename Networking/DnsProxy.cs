03abe8d8da7d47dee491abd2ac93c0ee4d29266d
Michael Yu <ItzWarty@gmail.com>
2014-12-06 22:40:45 -0800

    DnsProxy: Prioritize ipv4 address resolution.

--- Networking/DnsProxy.cs
+++ Networking/DnsProxy.cs
@@ -1,9 +1,16 @@
 ﻿using System.Net;
+using System.Net.Sockets;
 
 namespace ItzWarty.Networking {
    public class DnsProxy : IDnsProxy {
       public IPAddress ResolveIPAddress(string host) {
-         return Dns.GetHostEntry(host).AddressList[0];
+         var addresses = Dns.GetHostEntry(host).AddressList;
+         foreach (var address in addresses) {
+            if (address.AddressFamily == AddressFamily.InterNetwork) {
+               return address;
+            }
+         }
+         return addresses[0];
       }
    }
 }
