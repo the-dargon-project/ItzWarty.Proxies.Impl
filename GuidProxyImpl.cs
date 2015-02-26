3ad624ca95ac5ae438af0c607f8aa34d71e8a616
Michael Yu <ItzWarty@gmail.com>
2015-02-25 16:40:44 -0800

    Implement libwarty.proxies-api 0.0.18: Add GuidProxy.

---
+++ GuidProxyImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItzWarty {
+   public class GuidProxyImpl : GuidProxy {
+      public Guid NewGuid() {
+         return Guid.NewGuid();
+      }
+
+      public Guid Parse(string input) {
+         return Guid.Parse(input);
+      }
+
+      public Guid ParseExact(string input, string format) {
+         return Guid.ParseExact(input, format);
+      }
+
+      public bool TryParse(string input, out Guid result) {
+         return Guid.TryParse(input, out result);
+      }
+
+      public bool TryParseExact(string input, string format, out Guid result) {
+         return Guid.TryParseExact(input, format, out result);
+      }
+   }
+}
