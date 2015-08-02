665cdd6dffcf6a746dc52d8685e7fedd7991b95a
Michael Yu <ItzWarty@gmail.com>
2015-08-01 18:08:32 -0700

    Add Ryu Package for ItzWarty.Proxies.Impl.

---
+++ ItzWartyProxiesRyuPackage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dargon.Ryu;
+using ItzWarty.IO;
+using ItzWarty.Networking;
+using ItzWarty.Processes;
+using ItzWarty.Threading;
+
+namespace ItzWarty {
+   public class ItzWartyProxiesRyuPackage : RyuPackageV1 {
+      public ItzWartyProxiesRyuPackage() {
+         Singleton<GuidProxy, GuidProxyImpl>();
+
+         // IO
+         Singleton<InternalFileSystemProxy, FileSystemProxy>();
+         Singleton<IFileSystemProxy, FileSystemProxy>();
+         Singleton<IStreamFactory, StreamFactory>();
+
+         // Processes
+         Singleton<IProcessProxy, ProcessProxy>();
+
+         // Threading
+         Singleton<ISynchronizationFactory, SynchronizationFactory>();
+         Singleton<IThreadingFactory, ThreadingFactory>();
+         Singleton<IThreadingProxy, ThreadingProxy>();
+
+         // Networking
+         Singleton<IDnsProxy, DnsProxy>();
+         Singleton<INetworkingInternalFactory, NetworkingInternalFactory>();
+         Singleton<ISocketFactory, SocketFactory>();
+         Singleton<INetworkingProxy, NetworkingProxy>();
+         Singleton<ITcpEndPointFactory, TcpEndPointFactory>();
+      }
+   }
+}
---
+++ Tests/RyuPackageTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dargon.Ryu;
+using ItzWarty.IO;
+using ItzWarty.Networking;
+using ItzWarty.Processes;
+using ItzWarty.Threading;
+using NMockito;
+using Xunit;
+
+namespace ItzWarty {
+   public class RyuPackageTests : NMockitoInstance {
+      [Fact]
+      public void Run() {
+         var ryu = new RyuFactory().Create();
+         ryu.Setup();
+
+         AssertTrue(ryu.Get<GuidProxy>() is GuidProxyImpl);
+
+         // IO
+         AssertTrue(ryu.Get<InternalFileSystemProxy>() is FileSystemProxy);
+         AssertTrue(ryu.Get<IFileSystemProxy>() is FileSystemProxy);
+
+         // Processes
+         AssertTrue(ryu.Get<IProcessProxy>() is ProcessProxy);
+
+         // Threading
+         AssertTrue(ryu.Get<ISynchronizationFactory>() is SynchronizationFactory);
+         AssertTrue(ryu.Get<IThreadingFactory>() is ThreadingFactory);
+         AssertTrue(ryu.Get<IThreadingProxy>() is ThreadingProxy);
+
+         // Networking
+         AssertTrue(ryu.Get<IDnsProxy>() is DnsProxy);
+         AssertTrue(ryu.Get<INetworkingInternalFactory>() is NetworkingInternalFactory);
+         AssertTrue(ryu.Get<ISocketFactory>() is SocketFactory);
+         AssertTrue(ryu.Get<INetworkingProxy>() is NetworkingProxy);
+         AssertTrue(ryu.Get<ITcpEndPointFactory>() is TcpEndPointFactory);
+      }
+   }
+}
